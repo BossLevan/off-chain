ce7526052c2432c03bb249ea8affbb3210f9b24f
Kosi Anyaegbuna <59749496+BossLevan@users.noreply.github.com>
2025-05-10 00:32:47 +0100

    Rotated Keys

--- app/firebase_config.js
+++ app/firebase_config.js
@@ -7,14 +7,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAzQrWgF-kb0f4g_Qe5LwYvBWfyUTk55ww",
-  authDomain: "off-chain-c1547.firebaseapp.com",
-  projectId: "off-chain-c1547",
-  storageBucket: "off-chain-c1547.firebasestorage.app",
-  messagingSenderId: "196458158522",
-  appId: "1:196458158522:web:441dffba41992c675ee583",
-  measurementId: "G-DSGVXQ47TD",
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
