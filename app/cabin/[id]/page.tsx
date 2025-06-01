62dc7373710dfbf73122af5591be4a63d074f055
Kosi Anyaegbuna <59749496+BossLevan@users.noreply.github.com>
2025-06-01 02:03:34 +0100

    removed main, cold coded firebase storage img

--- app/cabin/[id]/page.tsx
+++ app/cabin/[id]/page.tsx
@@ -16,37 +16,45 @@ export async function generateMetadata({
   params: { id: string };
   searchParams: { img?: string };
 }): Promise<Metadata> {
-  const id = params.id;
-  const img = searchParams?.img;
+  try {
+    const id = params.id;
+    const img = searchParams?.img;
 
-  const client = new GraphQLClient(process.env.FLAUNCH_URL_MAINNET!);
-  const response = await client.request<{
-    collectionToken: SingleTokenDetailed;
-  }>(SINGLE_COIN_QUERY, { id: id });
+    const client = new GraphQLClient(process.env.FLAUNCH_URL_MAINNET!);
+    const response = await client.request<{
+      collectionToken: SingleTokenDetailed;
+    }>(SINGLE_COIN_QUERY, { id: id });
 
-  const ipfsHash = response?.collectionToken?.baseURI;
-  const cabinImageUrl = await convertIpfsToPinataUrl(ipfsHash);
-  const imageUrl = img || cabinImageUrl;
-
-  return {
-    openGraph: {
-      images: [imageUrl],
-    },
-    other: {
-      "fc:frame": JSON.stringify({
-        version: "vNext",
-        image: imageUrl,
-        button: {
-          title: "View Cabin",
-          action: {
-            type: "launch_frame",
-            name: "Cabin",
-            url: `https://off-chain.vercel.app/cabin/${id}`,
+    const ipfsHash = response?.collectionToken?.baseURI;
+    const cabinImageUrl = await convertIpfsToPinataUrl(ipfsHash);
+    const imageUrl =
+      "https://firebasestorage.googleapis.com/v0/b/off-chain-c1547.firebasestorage.app/o/contract-images%2F0x651d8fe5e89b38e94a09bf29c194e1726b676408%2Fimage-1-1747796245671?alt=media&token=7bf185f3-83b6-47f4-80e4-c0f3f9277bf9";
+    return {
+      openGraph: {
+        images: [imageUrl],
+      },
+      other: {
+        "fc:frame": JSON.stringify({
+          version: "next",
+          image: imageUrl,
+          button: {
+            title: "View Cabin",
+            action: {
+              type: "launch_frame",
+              name: "Cabin",
+              url: `https://off-chain.vercel.app/cabin/${id}`,
+            },
           },
-        },
-      }),
-    },
-  };
+        }),
+      },
+    };
+  } catch {
+    console.log("AN error occured");
+    return {
+      openGraph: {},
+      other: {},
+    };
+  }
 }
 
 export default function CabinDetailCabinPage({
@@ -56,9 +64,7 @@ export default function CabinDetailCabinPage({
   params: { id: string };
   searchParams: { img?: string };
 }) {
-  <main>
-    return <CabinDetailPage id={params.id} />;
-  </main>;
+  return <CabinDetailPage id={params.id} />;
 }
 
 // // Server-side rendering for meta tags
